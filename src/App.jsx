38c1247f545ca21916011305361f268710b80b68
jubi20000905 <66117384+jubi09@users.noreply.github.com>
2025-08-21 11:45:48 +0700

    update loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 import Navbar from './Ui/Navbar'
 import MobileMenu from './Ui/MobileMenu';
+import LoadingScreenAnimation from './Ui/LoadingScreenAnimation';
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div className='text-3xl font-bold underline'>
+    <>
+    <LoadingScreenAnimation />
       <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-    </div>
+    </>
   )
 }
 
